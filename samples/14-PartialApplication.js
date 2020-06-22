9887f567604d0a24ca3d6c80b47642af7864e3c1
Navid Khalili <nkhalili@live.com>
2020-06-22 19:40:51 -0400

    Partial application. Simplifying Currying

--- samples/14-PartialApplication.js
+++ samples/14-PartialApplication.js
@@ -2,8 +2,6 @@ const add = (x, y, z) => x + y + z;
 
 const addPartial = (x) => (y) => (z) => add(x, y, z);
 
-const add5 = addPartial(5);
-const add5and6 = add5(6);
-const sum = add5and6(7);
+const sum = addPartial(5)(6)(7);
 
 console.log(sum);
