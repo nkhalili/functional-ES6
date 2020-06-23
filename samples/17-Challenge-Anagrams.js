94add1a74890ae634682f4f607177bad6cbfdd3f
Navid Khalili <nkhalili@live.com>
2020-06-23 19:37:07 -0400

    Anagrams challenge

---
+++ samples/17-Challenge-Anagrams.js
@@ -0,0 +1,34 @@
+import words from 'an-array-of-english-words';
+
+const countOccurences = (arr) =>
+  arr.reduce(
+    (acc, str) => ({
+      ...acc,
+      [str]: acc[str] ? acc[str] + 1 : 1,
+    }),
+    {}
+  );
+
+const hasSameLetterCount = (word1, word2) => {
+  const word1Count = countOccurences(word1.split(''));
+  const word2Count = countOccurences(word2.split(''));
+
+  return (
+    Object.keys(word1Count).length === Object.keys(word2Count).length &&
+    Object.keys(word1Count).every(
+      (letter) => word1Count[letter] === word2Count[letter]
+    )
+  );
+};
+
+const findAnagrams = (word, allWords) => {
+  return allWords
+    .filter((entry) => hasSameLetterCount(word, entry))
+    .filter((anagram) => anagram !== word);
+};
+
+console.log(findAnagrams('cinema', words));
+
+/*
+    Expected output: ['iceman', 'anemic']
+*/
